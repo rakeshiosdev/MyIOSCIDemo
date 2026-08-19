cfa76b4ef9f3d72bb638217d40aff703f095a25b
Rakesh Kumar <rakeshiosdev28@gmail.com>
2026-08-19 11:03:54 +0530

    Add button

--- MyIOSCIDemo/ContentView.swift
+++ MyIOSCIDemo/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button("Press me") {
+                print("Button pressed")
+            }
         }
         .padding()
     }
